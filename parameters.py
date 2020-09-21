390ec6e9247ad5db7235d872885ccc8ae1439199
cousinskeeta <jacob.tadesse@gmail.com>
2020-09-21 19:30:00 -0400

    parameters

---
+++ parameters.py
@@ -0,0 +1,18 @@
+""" filename: parameters.py """
+
+
+# search query 
+job_title = "Data Scientist"
+company = "Spotify"
+search_query = f'site:linkedin.com/in/ AND "{job_title}" AND "{company}"'
+
+
+# file were scraped profile information will be stored  
+file_name = 'results_file.csv'
+
+# login credentials
+linkedin_username = 'username'
+linkedin_password = 'XXXXX'
+
+# hunter.io api credentials
+hunterio_api = 'secret_api'
